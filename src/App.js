3cc5b546873307edfb870e144adb28554bdd5b83
mgrinko <glukym@gmail.com>
2019-07-17 11:11:30 +0300

    handleChange

--- src/App.js
+++ src/App.js
@@ -1,19 +1,63 @@
 import React from 'react';
 import './App.css';
 
-import users from './api/users';
+// import users from './api/users';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Static list of todos</h1>
+class App extends React.Component {
+  state = {
+    firstName: '',
+    lastName: '',
+    isMarried: true,
+  };
 
-      <p>
-        <span>Users: </span>
-        {users.length}
-      </p>
-    </div>
-  );
+  handleFormSubmit = (event) => {
+    event.preventDefault();
+  };
+
+  handleFieldChange = (event) => {
+    const { name, value, checked, type } = event.target;
+
+    this.setState({
+      [name]: (type === 'checkbox') ? checked : value,
+    });
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <h1>Static list of todos</h1>
+
+        <form onSubmit={this.handleFormSubmit}>
+          <input
+            type="text"
+            name="firstName"
+            value={this.state.firstName}
+            placeholder="FirstName"
+            onChange={this.handleFieldChange}
+          />
+
+          <input
+            type="text"
+            name="lastName"
+            value={this.state.lastName}
+            placeholder="LastName"
+            onChange={this.handleFieldChange}
+          />
+
+          <input
+            type="checkbox"
+            name="isMarried"
+            checked={this.state.isMarried}
+            onChange={this.handleFieldChange}
+          />
+
+          <button type="submit">
+            Save
+          </button>
+        </form>
+      </div>
+    );
+  }
 }
 
 export default App;

81464a8477c4c902ebb8c7f4b0f51a2b870f752e
Ira <kalmykova_ira@ukr.net>
2019-07-18 12:27:01 +0300

    Updates

---
+++ precache-manifest.cbc7d8fcc9903c4fc974387f045fd79d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5b4fbf6d076fb248edae78826c7005db",
+    "url": "/react_add-todo-form/index.html"
+  },
+  {
+    "revision": "273ce20adb2349027edb",
+    "url": "/react_add-todo-form/static/css/main.48ada2f6.chunk.css"
+  },
+  {
+    "revision": "fc2e07ea8f90192b6544",
+    "url": "/react_add-todo-form/static/js/2.bc6991c7.chunk.js"
+  },
+  {
+    "revision": "273ce20adb2349027edb",
+    "url": "/react_add-todo-form/static/js/main.6be5d0ad.chunk.js"
+  },
+  {
+    "revision": "4953b3d381bec0cda918",
+    "url": "/react_add-todo-form/static/js/runtime~main.9d188fa2.js"
+  }
+]);
